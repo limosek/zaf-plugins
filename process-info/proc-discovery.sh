9fdf433a6d5a9e4effeb1d9cc315532798d9afdf
Lukas Macura <lukas@macura.cz>
2016-03-24 17:27:02 +0100

    process-info reworked

--- process-info/proc-discovery.sh
+++ process-info/proc-discovery.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-echo "{\n \"data\":[" ; /bin/ps --no-headers caux | /usr/bin/awk '{ print " { \"{#PSUSER}\":\"" $1 "\", \"{#PSNAME}\":\"" $11 "\" },"}' | /usr/bin/sort | /usr/bin/uniq | /bin/sed -e 's/\//\\\//g' -e '$s/.$//' ; echo " ]\n}"
+if [ "${ZAF_PKG}" = "opkg" ]; then
+	AWK='{ print $2" "$5; }'
+	PS="w"
+else
+	AWK='{ print $1" "$11; }'
+	PS="--no-headers caux"
+fi
+
+ps $PS | awk "$AWK" | sort | uniq | sed -e 's/\//\\\//g' -e '$s/.$//' | zaf_discovery '{#PSNAME}' '{#PSUSER}'
 
