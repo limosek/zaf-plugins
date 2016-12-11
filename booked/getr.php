065feeec04fad76832f133c5261f3ccc0f3f84db
Lukas Macura <lukas@macura.cz>
2016-12-11 13:06:36 +0100

    Added booked (phpscheduleit) plugin to get reservation count in interval

---
+++ booked/getr.php
@@ -0,0 +1,46 @@
+#!/usr/bin/env php
+<?php
+
+	error_reporting(0);
+	session_start();
+	$from=$argv[1];
+	$to=$argv[2];
+
+	define("YOURTIMEZONE",getenv("timezone"));
+	define("BOOKEDWEBSERVICESURL",getenv("url"));
+	require_once(__DIR__ . "/bookedapi.php");
+
+	if ($from) {
+		$from=New DateTime($from);
+		$from=$from->getTimestamp();
+	} else {
+		$from=time();
+	}
+	if ($to) {
+		$to=New DateTime($to);
+		$to=$to->getTimestamp();
+	} else {
+		$to=time()+3600;
+	}
+	
+	$bookedapiclient = new bookedapiclient(getenv("username"), getenv("password"));
+	$bookedapiclient-> authenticate();
+ 	$reservations=$bookedapiclient->getReservation(null,null,null,date("c",$from),date("c",$to));
+	
+	$cnt=0;
+	foreach ($reservations["reservations"] as $r) {
+		$start=New DateTime($r["startDate"]);
+		$start->setTimezone(New DateTimeZone(getenv("timezone")));
+		$end=New DateTime($r["endDate"]);
+		$end->setTimezone(New DateTimeZone(getenv("timezone")));
+		$sstart=$start->getTimestamp();
+		$send=$end->getTimestamp();
+		if ($sstart>=$from && $send<=$to) {
+			$cnt++;
+		}
+	}
+
+	echo $cnt;
+
+
+
